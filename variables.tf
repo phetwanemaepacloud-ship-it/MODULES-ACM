7b469fb75b03a7684a896477ee996cfe8ce3c057
phetwanemaepacloud-ship-it <phetwanemaepacloud@gmail.com>
2026-08-16 23:55:25 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,10 @@
+# ACM
+variable "domain_name" {
+  description = "Primary domain name"
+  type        = string
+}
 
+variable "alternative_names" {
+  description = "Alternative domain names (SANS)"
+  type        = string
+}  
